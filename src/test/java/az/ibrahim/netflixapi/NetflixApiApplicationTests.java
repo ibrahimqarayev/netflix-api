22602596262f108da1108033f32f5068b67b6bc7
ibrahimqarayev <qarayevibrahim7@mail.ru>
2024-05-02 23:17:53 +0400

    Initial commit

---
+++ src/test/java/az/ibrahim/netflixapi/NetflixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package az.ibrahim.netflixapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
